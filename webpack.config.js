f3a2fb24382f18f9a99d130b44f717f35fe7ba1c
Mateus Dal Bianco <mateus@dalbinaco.com>
2017-03-28 17:30:11 -0300

    Enable SASS source maps

--- webpack.config.js
+++ webpack.config.js
@@ -14,11 +14,12 @@ module.exports = {
     filename: "index_bundle.js",
     publicPath: '/'
   },
+  devtool: "source-map",
   module: {
     loaders: [
       { test: /\.js|jsx$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' },
       { test: /\.svg|jpg|png$/, exclude: /node_modules/, loader: 'file-loader' },
-      { test: /\.scss$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract({fallback: 'style-loader', use:['css-loader', 'sass-loader']}) }
+      { test: /\.scss$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract({fallback: 'style-loader', use:[{loader: 'css-loader', options: {sourceMap: true}}, {loader: 'sass-loader', options: {sourceMap: true}}]}) }
     ]
   },
   plugins: [

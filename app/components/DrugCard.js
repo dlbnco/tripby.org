917966cb856cd9b366c6607d090988588468fa21
dalbitacos <mateus@dalbinaco.com>
2017-05-20 16:36:21 -0300

    Add bottom margin to DrugCard component

--- app/components/DrugCard.js
+++ app/components/DrugCard.js
@@ -3,12 +3,12 @@ import React, { PropTypes } from 'react'
 class DrugCard extends React.Component{
   render () {
     return (
-      <div className="card">
+      <div className="card" style={{marginBottom: '1rem'}}>
         <div className="card-block">
           <div className="row">
             <div className="col-8">
-              <h4 className="tbBlue-text">MACONHA</h4>
-              <p style={{color: '#f06292'}}><strong>CANABINÓIDE</strong></p>
+              <h4 className="tbBlue-text">{this.props.drugName}</h4>
+              <p style={{color: '#f06292'}} className="text-uppercase"><strong>{this.props.drugClass}</strong></p>
             </div>
             <div className="col-4">
               <img src="http://tripby.org/wp-content/uploads/2016/01/2cb-1.svg" style={{width: '100%'}}/>

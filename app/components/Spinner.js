0ded426f2e9e767158073a8a1b7f72749b39654e
dalbitacos <mateus@dalbinaco.com>
2017-05-20 20:45:32 -0300

    Create Spinner component

---
+++ app/components/Spinner.js
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react'
+
+const Spinner = (props) => {
+  return (
+    <div className="spinner">
+      <div className="double-bounce1"></div>
+      <div className="double-bounce2"></div>
+    </div>
+  )
+}
+
+export default Spinner

4cb0ba1ac61395a190af15ef6521e13ac7bdf5a0
dalbitacos <mateus@dalbinaco.com>
2017-05-20 20:45:15 -0300

    Add info to DrugCard

--- app/components/DrugCard.js
+++ app/components/DrugCard.js
@@ -2,16 +2,25 @@ import React, { PropTypes } from 'react'
 
 class DrugCard extends React.Component{
   render () {
+    const moleculeStyle = {
+      backgroundImage: 'url(' + this.props.drugMolecule + ')',
+      backgroundSize: 'contain',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center'
+    }
     return (
       <div className="card" style={{marginBottom: '1rem'}}>
         <div className="card-block">
           <div className="row">
             <div className="col-8">
-              <h4 className="tbBlue-text">{this.props.drugName}</h4>
-              <p style={{color: '#f06292'}} className="text-uppercase"><strong>{this.props.drugClass}</strong></p>
+              <h3 className="tbBlue-text">{this.props.drugName}</h3>
+              <p className="mb-0">
+                <strong className="text-uppercase" style={{color: '#f06292'}}>{this.props.drugClass}</strong><br />
+                <i className="card__icon material-icons" aria-hidden="true">invert_colors</i>{this.props.drugDosage}<br />
+                <i className="card__icon material-icons" aria-hidden="true">timer</i>{this.props.drugDuration}
+              </p>
             </div>
-            <div className="col-4">
-              <img src="http://tripby.org/wp-content/uploads/2016/01/2cb-1.svg" style={{width: '100%'}}/>
+            <div className="col-4" style={moleculeStyle}>
             </div>
           </div>
         </div>

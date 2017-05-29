31db207333f863cd7726b26514dd86017ffbd457
dalbitacos <mateus@dalbinaco.com>
2017-05-29 20:40:09 -0300

    Update ListDrugs component

--- app/components/ListDrugs.js
+++ app/components/ListDrugs.js
@@ -18,7 +18,7 @@ class ListDrugs extends React.Component{
   theDrugs() {
     const drugs = this.state.drugObject.map( ( drug, index ) =>
     <div className="col-12 col-md-6" key={index}>
-      <DrugCard drugName={drug.title.rendered} drugClass={drug.acf.drug_type} drugMolecule={drug.acf.drug_molecule_grey} drugDosage={drug.acf.drug_dose} drugDuration={drug.acf.drug_duration}/>
+      <DrugCard slug={drug.slug} drugName={drug.title.rendered} drugClass={drug.acf.drug_type} drugMolecule={drug.acf.drug_molecule_grey} drugDosage={drug.acf.drug_dose} drugDuration={drug.acf.drug_duration}/>
     </div>
   )
   if (this.state.loading && this.state.error == null) {
@@ -29,33 +29,29 @@ class ListDrugs extends React.Component{
     return (
       <div className="col text-center">
         😥<br />
-        {this.state.error}
-      </div>
-    )
-  } else {
-    return drugs
-  }
+      {this.state.error}
+    </div>
+  )
+} else {
+  return drugs
+}
 }
 
 render () {
-  const drugs = this.state.drugObject.map( ( drug, index ) =>
-  <div className="col-12 col-md-6" key={index}>
-    <DrugCard drugName={drug.title.rendered} drugClass={drug.acf.drug_type} drugMolecule={drug.acf.drug_molecule_grey} drugDosage={drug.acf.drug_dose} drugDuration={drug.acf.drug_duration}/>
-  </div>
-)
-return (
-  <div>
-    <h4 className="text-uppercase text-center pb-3">Psicoativos</h4>
-    <div className="row">
-      {this.theDrugs()}
+  return (
+    <div>
+      <h4 className="text-uppercase text-center pb-3">Psicoativos</h4>
+      <div className="row">
+        {this.theDrugs()}
+      </div>
+      {this.state.loading ? null : <LinkButton text="Mais" link="/psicoativos" icon="trending_flat" />}
+      {this.state.loading ? null : <LinkButton text="Adicionar novo" link="/psicoativos" icon="add" secondary/>}
     </div>
-    {this.state.loading ? null : <LinkButton text="Mais" link="/psicoativos" arrow />}
-  </div>
-)
+  )
 }
 
 componentDidMount() {
-  var self = this;
+  var that = this;
   // first call
   // get drug info from WordPress API
   axios.get('http://tripby.org/wp-json/wp/v2/pages', {
@@ -66,12 +62,12 @@ componentDidMount() {
     }
   })
   .then(function (response) {
-    // set state with info form first call
+    // set state with info from response
     console.log(response.data)
-    self.setState({drugObject: response.data, loading: false})
+    that.setState({drugObject: response.data, loading: false})
   })
   .catch(function (error) {
-    self.setState({error: 'Desculpe, não foi possível fazer o corre das informações'})
+    that.setState({error: 'Desculpe, não foi possível fazer o corre das informações'})
   });
 
 

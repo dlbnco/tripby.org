dc16df053273e76e65fba80c86d598d760f50b64
dalbitacos <mateus@dalbinaco.com>
2017-05-29 20:39:53 -0300

    Update LinkButton to accept any icon

--- app/components/LinkButton.js
+++ app/components/LinkButton.js
@@ -2,14 +2,10 @@ import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
 
 const LinkButton = (props) => {
-  let arrow = null;
-  if (props.arrow) {
-    arrow = <i className="material-icons" style={{marginLeft: '0.5rem'}} aria-hidden="true">trending_flat</i>;
-  } else arrow = '';
   return (
-    <div className="d-inline-block"><Link to={props.link} className={'btn d-flex align-items-center' + ' ' + (props.campaign ? 'btn--campaign' : '') + ' ' + (props.centered ? 'mx-auto' : '')}>
+    <div className="d-inline-block"><Link to={props.link} className={'btn d-flex align-items-center ' + (props.campaign ? 'btn--campaign' : '') + ' ' + (props.centered ? 'mx-auto' : '') + ' ' + (props.secondary ? 'btn-secondary' : '')}>
       {props.text}
-      {arrow}
+      {props.icon ? <i className="material-icons" style={{marginLeft: '0.5rem'}} aria-hidden="true">{props.icon}</i> : null}
     </Link></div>
   )
 }

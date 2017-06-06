45fd12fd2c1f0a0795f2b5c884924db6b87d0037
dalbitacos <mateus@dalbinaco.com>
2017-06-05 23:00:36 -0300

    Fix fat arrow identation

--- app/components/ListDrugs.js
+++ app/components/ListDrugs.js
@@ -16,62 +16,57 @@ class ListDrugs extends React.Component{
   }
 
   theDrugs() {
-    const drugs = this.state.drugObject.map( ( drug, index ) =>
-    <div className="col-12 col-md-6" key={index}>
-      <DrugCard slug={drug.slug} drugName={drug.title.rendered} drugClass={drug.acf.drug_type} drugMolecule={drug.acf.drug_molecule_grey} drugDosage={drug.acf.drug_dose} drugDuration={drug.acf.drug_duration}/>
-    </div>
-  )
-  if (this.state.loading && this.state.error == null) {
-    return (
-      <Spinner className="mx-auto" />
-    )
-  } else if (this.state.error !== null) {
-    return (
-      <div className="col text-center">
-        😥<br />
-      {this.state.error}
-    </div>
-  )
-} else {
-  return drugs
-}
-}
-
-render () {
-  return (
-    <div>
-      <h4 className="text-uppercase text-center pb-3">Psicoativos</h4>
-      <div className="row">
-        {this.theDrugs()}
+    const drugs = this.state.drugObject.map( ( drug, index ) => (
+      <div className="col-12 col-md-6" key={index}>
+        <DrugCard slug={drug.slug} drugName={drug.title.rendered} drugClass={drug.acf.drug_type} drugMolecule={drug.acf.drug_molecule_grey} drugDosage={drug.acf.drug_dose} drugDuration={drug.acf.drug_duration}/>
       </div>
-      {this.state.loading ? null : <LinkButton text="Mais" link="/psicoativos" icon="trending_flat" />}
-      {this.state.loading ? null : <LinkButton text="Adicionar novo" link="/psicoativos" icon="add" secondary/>}
-    </div>
-  )
-}
+    ))
+    if (this.state.loading && this.state.error == null) {
+      return (
+        <Spinner className="mx-auto" />
+      )
+    } else if (this.state.error !== null) {
+      return (
+        <div className="col text-center">
+          😥<br />{this.state.error}
+        </div>
+      )
+    } else return drugs
+  }
 
-componentDidMount() {
-  var that = this;
-  // first call
-  // get drug info from WordPress API
-  axios.get('http://tripby.org/wp-json/wp/v2/pages', {
-    params: {
-      order: 'desc',
-      per_page: this.props.perPage,
-      parent: 1484
-    }
-  })
-  .then(function (response) {
-    // set state with info from response
-    console.log(response.data)
-    that.setState({drugObject: response.data, loading: false})
-  })
-  .catch(function (error) {
-    that.setState({error: 'Desculpe, não foi possível fazer o corre das informações'})
-  });
+  render () {
+    return (
+      <div>
+        <div className="row">
+          {this.theDrugs()}
+        </div>
+        {this.state.loading ? null : <LinkButton text="Mais" link="/psicoativos" icon="trending_flat" />}
+        {this.state.loading ? null : <LinkButton text="Adicionar novo" link="/psicoativos" icon="add" secondary/>}
+      </div>
+    )
+  }
 
-
-}
+  componentDidMount() {
+    const that = this
+    // first call
+    // get drug info from WordPress API
+    axios.get('http://tripby.org/wp-json/wp/v2/pages', {
+      params: {
+        order: 'desc',
+        orderby: 'date',
+        per_page: this.props.perPage,
+        parent: 1484
+      }
+    })
+    .then(function (response) {
+      // set state with info from response
+      console.log(response.data)
+      that.setState({drugObject: response.data, loading: false})
+    })
+    .catch(function (error) {
+      that.setState({error: 'Desculpe, não foi possível fazer o corre das informações'})
+    });
+  }
 
 
 }

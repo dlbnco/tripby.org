396e4cf8eec4ce1c6538293a4de624d8054e8cc6
Mateus Dal Bianco Ferreira <marketing2@softin.com.br>
2017-08-14 11:31:08 -0300

    Move Spinner'

--- react-boilerplate/app/components/Spinner/index.js
+++ react-boilerplate/app/components/Spinner/index.js
@@ -10,7 +10,9 @@ import React from 'react'
 
 function Spinner() {
   return (
-    <div>
+    <div className="spinner">
+      <div className="double-bounce1"></div>
+      <div className="double-bounce2"></div>
     </div>
   )
 }

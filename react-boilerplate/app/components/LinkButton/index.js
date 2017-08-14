92302b83878389892da651e06a0ca49e17cbccce
Mateus Dal Bianco Ferreira <marketing2@softin.com.br>
2017-08-14 11:28:27 -0300

    Move LinkButton

--- react-boilerplate/app/components/LinkButton/index.js
+++ react-boilerplate/app/components/LinkButton/index.js
@@ -5,18 +5,28 @@
 */
 
 import React from 'react'
-// import styled from 'styled-components';
+import PropTypes from 'prop-types'
+import { Link } from 'react-router'
 
 
-function LinkButton() {
+function LinkButton(props) {
   return (
-    <div>
+    <div className="d-inline-block">
+      <Link to={props.link} className={`btn d-flex align-items-center ${props.campaign ? 'btn--campaign' : ''} ${props.centered ? 'mx-auto' : ''} ${(props.secondary ? 'btn-secondary' : '')}`}>
+        {props.text}
+        {props.icon ? <i className="material-icons" style={{ marginLeft: '0.5rem' }} aria-hidden="true">{props.icon}</i> : null}
+      </Link>
     </div>
   )
 }
 
 LinkButton.propTypes = {
-
+  text: PropTypes.string,
+  link: PropTypes.string,
+  campaign: PropTypes.bool,
+  icon: PropTypes.string,
+  centered: PropTypes.bool,
+  secondary: PropTypes.bool,
 }
 
 export default LinkButton

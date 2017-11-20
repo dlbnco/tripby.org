501f26550ff11a3a428f45927723dad083e28fcf
Mateus Dal Bianco <mateus@dalbinaco.com>
2017-11-20 16:37:36 -0200

    Make Apollo queries with user token

--- app/app.js
+++ app/app.js
@@ -19,7 +19,8 @@ import 'sanitize.css/sanitize.css'
 
 // Import Apollo stuff
 import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
+import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloProvider } from 'react-apollo'
 
@@ -71,8 +72,23 @@ const rootRoute = {
 }
 
 // Setup Apollo
+const httpLink = createHttpLink({
+  uri: 'https://api.graph.cool/simple/v1/tripby',
+})
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token')
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
+    },
+  }
+})
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'https://api.graph.cool/simple/v1/tripby' }),
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
 

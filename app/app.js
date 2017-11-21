068bab03bf30233d6794aa5e554194e8c4409b5b
Mateus Dal Bianco <mateus@dalbinaco.com>
2017-11-21 12:14:24 -0200

    Rebuild favicons

--- app/app.js
+++ app/app.js
@@ -35,8 +35,6 @@ import LanguageProvider from 'containers/LanguageProvider'
 
 // Load the favicon, the manifest.json file and the .htaccess file
 /* eslint-disable import/no-unresolved, import/extensions */
-import '!file-loader?name=[name].[ext]!./favicon.ico'
-import '!file-loader?name=[name].[ext]!./manifest.json'
 import 'file-loader?name=[name].[ext]!./.htaccess'
 /* eslint-enable import/no-unresolved, import/extensions */
 

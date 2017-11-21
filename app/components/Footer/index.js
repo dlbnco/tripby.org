9871e232aa0a60049080d6c309451156e54809dc
Mateus Dal Bianco <mateus@dalbinaco.com>
2017-11-21 12:14:34 -0200

    Update Footer

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -11,14 +11,14 @@ import React from 'react'
 function Footer() {
   return (
     <div>
-      <section className="py-5" style={{ background: '#f5f5f5' }}>
+      <section className="py-4" style={{ background: '#f5f5f5' }}>
         <div className="container">
           <div className="row align-items-center">
             <ul className="col-12 col-md-6 d-flex mb-0 flex-column flex-md-row">
-              <li className="mr-5" style={{ listStyleType: 'none' }}><a href="https://www.facebook.com/tripby">Facebook</a></li>
-              <li className="mr-5" style={{ listStyleType: 'none' }}><a href="https://www.facebook.com/tripby">Twitter</a></li>
-              <li className="mr-5" style={{ listStyleType: 'none' }}><a href="https://www.facebook.com/tripby">Instagram</a></li>
-              <li className="mr-5" style={{ listStyleType: 'none' }}><a href="https://www.facebook.com/tripby">GitHub</a></li>
+              <li className="mr-5" style={{ listStyleType: 'none' }}><a href="https://tripby-slack.herokuapp.com/">Slack</a></li>
+              <li className="mr-5" style={{ listStyleType: 'none' }}><a href="https://twitter.com/tripby_">Twitter</a></li>
+              <li className="mr-5" style={{ listStyleType: 'none' }}><a href="https://www.instagram.com/tripby_/">Instagram</a></li>
+              <li className="mr-5" style={{ listStyleType: 'none' }}><a href="https://github.com/tripby/">GitHub</a></li>
             </ul>
           </div>
         </div>

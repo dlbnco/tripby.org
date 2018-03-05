316bf6475a7b3decf914e9d4bf06f72949e36acc
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-03-04 21:36:56 -0300

    Start transfering pagination control to query string

--- app/containers/AllDrugs/index.js
+++ app/containers/AllDrugs/index.js
@@ -5,11 +5,13 @@
  */
 
 import React, { PropTypes } from 'react'
+import { browserHistory } from 'react-router'
 import { connect } from 'react-redux'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import Helmet from 'react-helmet'
 import { FormattedMessage } from 'react-intl'
+import qs from 'query-string'
 import { createStructuredSelector } from 'reselect'
 import makeSelectAllDrugs from './selectors'
 import ListDrugs from '../../components/ListDrugs'
@@ -27,20 +29,36 @@ export class AllDrugs extends React.Component { // eslint-disable-line react/pre
       },
     }
     this.handlePagination = this.handlePagination.bind(this)
+    this.updateQuery = this.updateQuery.bind(this)
+  }
+  updateQuery() {
+    const query = qs.stringify(this.state.navigation)
+    browserHistory.push({
+      pathname: '/drugs',
+      search: `?${query}`,
+    })
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log('updated')
+    if (prevState !== this.state) {
+      this.updateQuery()
+    }
   }
   handlePagination(page) {
     const navigation = this.state.navigation
-    navigation.skip = navigation.limit * (page)
     navigation.page = page
     this.setState({
       navigation,
     })
   }
   render() {
-    const nav = this.state.navigation
+    const nav = this.props.location.search !== '' ? this.props.location.query : this.state.navigation
+    const currentPage = Number(nav.page)
+    const limit = Number(nav.limit)
+    const skip = limit * (currentPage)
     const pages = []
     if (!this.props.data.loading) {
-      for (let i = 0; i < Math.ceil(this.props.data._allDrugsMeta.count / nav.limit); i += 1) {
+      for (let i = 0; i < Math.ceil(this.props.data._allDrugsMeta.count / limit); i += 1) {
         pages.push(i + 1)
       }
     }
@@ -57,15 +75,15 @@ export class AllDrugs extends React.Component { // eslint-disable-line react/pre
         </PageHeader>
         <section className="py-4">
           <div className="container">
-            <ListDrugs limit={nav.limit} orderBy={nav.orderBy} skip={nav.skip} />
+            <ListDrugs limit={limit} orderBy={nav.orderBy} skip={skip} />
             <nav aria-label="Page navigation example">
               <ul className="pagination list-unstyled">
-                <li className="page-item"><button disabled={nav.page === 0} onClick={() => this.handlePagination(nav.page - 1)} className="page-link" href="#">Anterior</button></li>
+                <li className="page-item"><button disabled={currentPage === 0} onClick={() => this.handlePagination(currentPage - 1)} className="page-link" href="#">Anterior</button></li>
                 { pages.map((page) => (
-                  <li className={`page-item ${nav.page === (page - 1) ? 'active' : ''}`}><button onClick={() => this.handlePagination(page - 1)} className="page-link" href="#">{page}</button></li>
+                  <li className={`page-item ${currentPage === (page - 1) ? 'active' : ''}`}><button onClick={() => this.handlePagination(page - 1)} className="page-link" href="#">{page}</button></li>
                   ))
                 }
-                <li className="page-item"><button className="page-link" href="#">Próximo</button></li>
+                <li className="page-item"><button onClick={() => this.handlePagination(currentPage + 1)} className="page-link" href="#">Próximo</button></li>
               </ul>
             </nav>
           </div>

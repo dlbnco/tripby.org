0093e71ffab85f09f378c72171e12acc97d178a2
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-04-16 19:28:43 -0300

    Create /search route

--- app/routes.js
+++ app/routes.js
@@ -86,6 +86,14 @@ export default function createRoutes(store) {
           .catch(errorLoading)
       },
     }, {
+      path: '/search',
+      name: 'search',
+      getComponent(location, cb) {
+        import('components/Search')
+          .then(loadModule(cb))
+          .catch(errorLoading)
+      },
+    }, {
       path: '*',
       name: 'notfound',
       getComponent(nextState, cb) {

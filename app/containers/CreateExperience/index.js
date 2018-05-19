2193d9e60b56320cbce4eb263042433196681444
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-05-19 18:46:44 -0300

    Show GateKeeper if user is not logged in

--- app/containers/CreateExperience/index.js
+++ app/containers/CreateExperience/index.js
@@ -22,6 +22,7 @@ import Spinner from '../../components/Spinner'
 import Alert from '../../components/Alert'
 import Button from '../../components/Button'
 import Badge from '../../components/Badge'
+import GateKeeper from '../../components/GateKeeper'
 
 export class CreateExperience extends React.Component { // eslint-disable-line react/prefer-stateless-function
   constructor() {
@@ -86,91 +87,95 @@ export class CreateExperience extends React.Component { // eslint-disable-line r
           <FormattedMessage {...messages.header} />
         </PageHeader>
         <div className="container">
-          <section className="py-4 py-md-5">
+          {this.props.userId ? (
+            <section className="py-4 py-md-5">
 
-            <Mutation
-              mutation={CREATE_EXPERIENCE}
-              onCompleted={(data) => { browserHistory.push(`/experiences/create/success?id=${data.createExperience.id}`) }}
-              onError={() => { window.scrollTo(0, 0) }}
-            >
-              {(createExperience, { loading, error }) => (
-                <form
-                  onSubmit={(e) => {
-                    e.preventDefault()
-                    createExperience({
-                      variables: {
-                        title: e.target.title.value,
-                        story: this.state.story,
-                        drugsIds: this.state.selectedDrugs.reduce((arr, drug) => {
-                          arr.push(drug.id)
-                          return arr
-                        }, []),
-                        authorId: this.props.userId,
-                      },
-                    })
+              <Mutation
+                mutation={CREATE_EXPERIENCE}
+                onCompleted={(data) => { browserHistory.push(`/experiences/create/success?id=${data.createExperience.id}`) }}
+                onError={() => { window.scrollTo(0, 0) }}
+              >
+                {(createExperience, { loading, error }) => (
+                  <form
+                    onSubmit={(e) => {
+                      e.preventDefault()
+                      createExperience({
+                        variables: {
+                          title: e.target.title.value,
+                          story: this.state.story,
+                          drugsIds: this.state.selectedDrugs.reduce((arr, drug) => {
+                            arr.push(drug.id)
+                            return arr
+                          }, []),
+                          authorId: this.props.userId,
+                        },
+                      })
+                    }
                   }
-                }
-                >
-                  {error ?
-                    <div className="my-4"><Alert type="danger">
-                      <h5>Algo deu errado</h5>
-                      <ul className="m-0">
-                        {error.graphQLErrors.map((err) => (
-                          <li>{err.message}</li>
-                      ))}
-                      </ul>
-                    </Alert></div>
-                  : null}
-                  <div className="row">
-                    <div className="col-12 col-md-8">
-                      <div className="form-group">
-                        <label htmlFor="title"><h5>Título da experiência</h5></label>
-                        <input onChange={this.handleInputs} type="text" id="title" name="title" className="form-control form-control-lg" />
-                      </div>
-                      <div className="form-group">
-                        <label htmlFor="story"><h5>Conte a história da experiência</h5></label>
-                        {/* <textarea ref={this.storyTextarea} name="story" id="story" className="form-control" onChange={this.handleInputs}></textarea> */}
-                        <SimpleMDE value={this.state.story} id="story" options={{ spellChecker: false, forceSync: true }} onChange={(e) => this.handleInputs({ target: { value: e, name: 'story' } })} />
-                      </div>
-
-                    </div>
-                    <div className="col-12 col-md-4">
-                      {!this.props.data.loading ? (
+                  >
+                    {error ?
+                      <div className="my-4"><Alert type="danger">
+                        <h5>Algo deu errado</h5>
+                        <ul className="m-0">
+                          {error.graphQLErrors.map((err) => (
+                            <li>{err.message}</li>
+                        ))}
+                        </ul>
+                      </Alert></div>
+                    : null}
+                    <div className="row">
+                      <div className="col-12 col-md-8">
                         <div className="form-group">
-                          <label htmlFor="filter"><h5><FormattedMessage {...messages.drugSelection} /></h5></label>
-                          <div className="card d-block mb-3">
-                            <div className="card-body">
-                              <input className="form-control" name="filter" type="text" placeholder="Filtrar..." onChange={this.handleInputs} value={this.state.filter} />
-                            </div>
-                            <ul className="list-group list-group-flush d-block" style={{ maxHeight: 240, overflowY: 'auto' }}>
-                              {drugs.map((drug) =>
-                                <button type="button" key={drug.id} onClick={() => this.handleSelect(drug)} className={drugButton(drug)}>
-                                  {drug.name}
-                                  <FeatherIcon icon={this.state.selectedDrugs.indexOf(drug) >= 0 ? 'check-circle' : 'circle'} size={24} />
-                                </button>
-                              )}
-                            </ul>
-                          </div>
-                          {this.state.selectedDrugs.map((drug) =>
-                            <div key={drug.id} className="d-inline-flex mr-2 mb-2"><Badge close={() => this.handleSelect(drug)} bg="pinkLight">{drug.name}</Badge></div>
-                          )}
+                          <label htmlFor="title"><h5>Título da experiência</h5></label>
+                          <input onChange={this.handleInputs} type="text" id="title" name="title" className="form-control form-control-lg" />
                         </div>
-                        )
-                          : <Spinner />}
-                    </div>
-                    <div className="col-12">
-                      <div className="d-inline-flex align-items-center">
-                        <Button btnStyle="primary" type="submit" disabled={loading || this.state.selectedDrugs.length === 0 || this.state.title === '' || this.state.story === ''}>
-                          Continuar
-                        </Button>
-                        {loading ? <span className="ml-2"><Spinner /></span> : null}
+                        <div className="form-group">
+                          <label htmlFor="story"><h5>Conte a história da experiência</h5></label>
+                          {/* <textarea ref={this.storyTextarea} name="story" id="story" className="form-control" onChange={this.handleInputs}></textarea> */}
+                          <SimpleMDE value={this.state.story} id="story" options={{ spellChecker: false, forceSync: true }} onChange={(e) => this.handleInputs({ target: { value: e, name: 'story' } })} />
+                        </div>
+
+                      </div>
+                      <div className="col-12 col-md-4">
+                        {!this.props.data.loading ? (
+                          <div className="form-group">
+                            <label htmlFor="filter"><h5><FormattedMessage {...messages.drugSelection} /></h5></label>
+                            <div className="card d-block mb-3">
+                              <div className="card-body">
+                                <input className="form-control" name="filter" type="text" placeholder="Filtrar..." onChange={this.handleInputs} value={this.state.filter} />
+                              </div>
+                              <ul className="list-group list-group-flush d-block" style={{ maxHeight: 240, overflowY: 'auto' }}>
+                                {drugs.map((drug) =>
+                                  <button type="button" key={drug.id} onClick={() => this.handleSelect(drug)} className={drugButton(drug)}>
+                                    {drug.name}
+                                    <FeatherIcon icon={this.state.selectedDrugs.indexOf(drug) >= 0 ? 'check-circle' : 'circle'} size={24} />
+                                  </button>
+                                )}
+                              </ul>
+                            </div>
+                            {this.state.selectedDrugs.map((drug) =>
+                              <div key={drug.id} className="d-inline-flex mr-2 mb-2"><Badge close={() => this.handleSelect(drug)} bg="pinkLight">{drug.name}</Badge></div>
+                            )}
+                          </div>
+                          )
+                            : <Spinner />}
+                      </div>
+                      <div className="col-12">
+                        <div className="d-inline-flex align-items-center">
+                          <Button btnStyle="primary" type="submit" disabled={loading || this.state.selectedDrugs.length === 0 || this.state.title === '' || this.state.story === ''}>
+                            Continuar
+                          </Button>
+                          {loading ? <span className="ml-2"><Spinner /></span> : null}
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </form>
-              )}
-            </Mutation>
-          </section>
+                  </form>
+                )}
+              </Mutation>
+            </section>
+          ) : (
+            <GateKeeper location={this.props.location} />
+          )}
         </div>
       </div>
     )
@@ -182,6 +187,7 @@ CreateExperience.propTypes = {
   dispatch: PropTypes.func.isRequired, //eslint-disable-line
   data: PropTypes.object,
   userId: PropTypes.string,
+  location: PropTypes.object,
 }
 
 

c78a02deff3bcdd7f8721dadec42521519f37af6
Mateus Dal Bianco <mateus@dalbinaco.com>
2019-05-11 15:45:54 -0300

    Update index page
    - Use SubstanceCard
    - Link to /substances
    - Get random offsets

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,44 @@
 import React from 'react';
 import { useQuery } from 'react-apollo-hooks';
-import { Card, StyledBody } from 'baseui/card';
 import { Flex, Box } from 'rebass';
+import Link from 'next/link';
+
 import Container from '../components/Container';
+import Hero from '../components/Hero';
+import { FormattedMessage } from 'react-intl';
+import Text from '../components/Text';
 import GET_SUBSTANCES from '../queries/substances';
+import SubstanceCard from '../components/Substance/Card';
 
 const offset = Math.floor(Math.random() * Math.floor(200));
 
 export default () => {
-  const { data } = useQuery(GET_SUBSTANCES, { variables: { limit: 6 } });
+  const { data } = useQuery(GET_SUBSTANCES, {
+    variables: {
+      limit: 9,
+      offset,
+    },
+  });
   return (
-    <Container>
-      <Flex m={-2} flexWrap="wrap">
-        {data.substances &&
-          data.substances.map(sub => (
-            <Box p={2} width={[1, 1 / 2, 1 / 3]} key={sub.name}>
-              <Card title={sub.name}>
-                <StyledBody>{sub.class.psychoactive}</StyledBody>
-              </Card>
-            </Box>
-          ))}
-      </Flex>
-    </Container>
+    <>
+      <Hero mb={[3, 4]} />
+      <Container>
+        <Flex m={-2} flexWrap="wrap" mb={3}>
+          {data.substances &&
+            data.substances.map(sub => (
+              <Box p={2} width={[1, null, 1 / 2, 1 / 3]} key={sub.name}>
+                <SubstanceCard substance={sub} />
+              </Box>
+            ))}
+        </Flex>
+        <Link href="/substances">
+          <a>
+            <Text variant="secondary">
+              <FormattedMessage id="Home.viewAll" />
+            </Text>
+          </a>
+        </Link>
+      </Container>
+    </>
   );
 };

480d88d347d195ac5a800838577db08dcf8b92ce
Mateus Dal Bianco <mateus@dalbinaco.com>
2019-05-11 15:45:00 -0300

    Fix constant names

--- components/Text/index.js
+++ components/Text/index.js
@@ -1,11 +1,11 @@
 import { Text as CoreText } from 'rebass';
 import styled from 'styled-components';
 
-const Wrapper = styled(CoreText)`
+const Text = styled(CoreText)`
   color: ${({ color, theme }) => color || theme.textColor};
 `;
 
-Wrapper.defaultProps = {
+Text.defaultProps = {
   variant: 'primary',
 };
 

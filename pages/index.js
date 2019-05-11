7edcab9a6c368405603d917d5ed96e7043829ee2
Mateus Dal Bianco <mateus@dalbinaco.com>
2019-05-11 13:59:17 -0300

    List substances with Card

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
-import { Button } from 'baseui/button';
+import gql from 'graphql-tag';
+import { useQuery } from 'react-apollo-hooks';
+import { Card, StyledBody } from 'baseui/card';
+import { Flex, Box } from 'rebass';
+import Container from '../components/Container';
 
-export default () => (
-  <div>
-    <Button>Hello</Button>
-  </div>
-);
+const GET_SUBSTANCES = gql`
+  query getSubstances($limit: Int) {
+    substances(limit: $limit) {
+      name
+      images {
+        image
+      }
+      class {
+        psychoactive
+      }
+    }
+  }
+`;
+
+export default () => {
+  const { data } = useQuery(GET_SUBSTANCES, { variables: { limit: 6 } });
+  return (
+    <Container>
+      <Flex m={-2} flexWrap="wrap">
+        {data.substances &&
+          data.substances.map(sub => (
+            <Box p={2} width={[1, 1 / 2, 1 / 3]} key={sub.name}>
+              <Card title={sub.name}>
+                <StyledBody>{sub.class.psychoactive}</StyledBody>
+              </Card>
+            </Box>
+          ))}
+      </Flex>
+    </Container>
+  );
+};

bfb0dc3ca4dd98e88e5815096990f81a5b2c9bcc
Mateus Dal Bianco <mateus@dalbinaco.com>
2019-05-11 15:45:23 -0300

    Remove text-color property

--- components/Layout/GlobalStyle.js
+++ components/Layout/GlobalStyle.js
@@ -1,9 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
-import { textColor } from '../../lib/theme';
 
 const GlobalStyle = createGlobalStyle`
-  * {font-family: 'Barlow', sans-serif !important; color: ${({ theme }) =>
-    textColor({ theme, variant: 'primary' })};}
+  * {font-family: 'Barlow', sans-serif !important;}
   a {
     color: inherit;
     text-decoration: none;

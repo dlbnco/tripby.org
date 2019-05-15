0642bd843d79d68e83e1d24775c96e2998513d1f
Mateus Dal Bianco <mateus@dalbinaco.com>
2019-05-15 19:00:29 -0300

    Add messages

--- components/IntlProvider/messages.js
+++ components/IntlProvider/messages.js
@@ -2,6 +2,10 @@ const messages = {
   en: {
     'Hero.title': 'Read the label.',
     'Home.viewAll': 'View all substances →',
+    'Substance.psychoactiveClass': 'Psychoactive class',
+    'Substance.chemicalClass': 'Chemical class',
+    'Substance.roas': 'Routes of administration',
+    'Substance.dosage': 'Dosage',
   },
 };
 

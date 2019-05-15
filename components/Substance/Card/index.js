7d6e5d0f18d80a18c51275cc1d16fb84edd3c952
Mateus Dal Bianco <mateus@dalbinaco.com>
2019-05-15 19:02:40 -0300

    Update Substance/Card

--- components/Substance/Card/index.js
+++ components/Substance/Card/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, StyledBody } from 'baseui/card';
+import Link from 'next/link';
+
+import Card from '../../Card';
+import Heading from '../../Heading';
+import Text from '../../Text';
+import { colors } from '../../../lib/constants';
 
 const SubstanceCard = ({ substance }) => {
   return (
-    <Card title={substance.name}>
-      {substance.class && substance.class.psychoactive && (
-        <StyledBody>{substance.class.psychoactive.join(' — ')}</StyledBody>
-      )}
-    </Card>
+    <Link href={`substances?name=${substance.name}`}>
+      <a>
+        <Card>
+          <Heading mb={2} fontSize={2} fontWeight="500">
+            {substance.name}
+          </Heading>
+          {substance.class && substance.class.psychoactive && (
+            <Text color={colors.persianGreen}>
+              {substance.class.psychoactive.join('/')}
+            </Text>
+          )}
+        </Card>
+      </a>
+    </Link>
   );
 };
 
-SubstanceCard.propTypes = {};
+SubstanceCard.propTypes = {
+  substance: PropTypes.object.isRequired,
+};
 
 export default SubstanceCard;

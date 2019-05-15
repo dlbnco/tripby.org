29a6185eda9600e8bdb937050405e5c5bf057a3f
Mateus Dal Bianco <mateus@dalbinaco.com>
2019-05-15 19:02:53 -0300

    Build custom Input component

--- components/Input/index.js
+++ components/Input/index.js
@@ -1,8 +1,27 @@
-import { Input } from 'baseui/input';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { space } from 'styled-system';
 
-export default styled(Input)`
+const Input = styled.input`
   ${space}
-  border-color: ${({ theme }) => theme.colors.purpleHeart};
+  ${({ theme: { borderColor, borderWidth, borderRadius } }) => css`
+    border-radius: ${borderRadius};
+    border-width: ${borderWidth.regular};
+    border-style: solid;
+    border-color: ${borderColor};
+  `}
+  transition: 0.2s;
+  &:focus {
+    outline: 0;
+    ${({ theme: { colors } }) => css`
+      box-shadow: 0 0 4px 0 ${colors.screaminGreen};
+      border-color: ${colors.heliotrope};
+    `};
+  }
 `;
+
+Input.defaultProps = {
+  p: 2,
+  variant: 'primary',
+};
+
+export default Input;

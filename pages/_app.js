c1ee9c9e2fe66a1c6dc5188b7b8b4103d2284589
Mateus Dal Bianco <mateus@dalbinaco.com>
2019-05-15 19:01:43 -0300

    Remove baseui and styletron; add lodash

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import App, { Container } from 'next/app';
 import React from 'react';
-import { Provider as StyletronProvider } from 'styletron-react';
 import withApolloClient from '../lib/with-apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
 import { ThemeProvider } from 'styled-components';
 
-import { styletron } from '../lib/styletron';
 import Layout from '../components/Layout';
 import { defaultTheme } from '../lib/theme';
 import IntlProvider from '../components/IntlProvider';
@@ -18,15 +16,13 @@ class MyApp extends App {
       <Container>
         <ApolloProvider client={apolloClient}>
           <ApolloHooksProvider client={apolloClient}>
-            <StyletronProvider value={styletron}>
-              <ThemeProvider theme={defaultTheme}>
-                <IntlProvider>
-                  <Layout>
-                    <Component {...pageProps} />
-                  </Layout>
-                </IntlProvider>
-              </ThemeProvider>
-            </StyletronProvider>
+            <ThemeProvider theme={defaultTheme}>
+              <IntlProvider>
+                <Layout>
+                  <Component {...pageProps} />
+                </Layout>
+              </IntlProvider>
+            </ThemeProvider>
           </ApolloHooksProvider>
         </ApolloProvider>
       </Container>
--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
-import { Provider as StyletronProvider } from 'styletron-react';
 import { ServerStyleSheet } from 'styled-components';
-import { styletron } from '../lib/styletron';
 
 class MyDocument extends Document {
   static getInitialProps(props) {
     const sheet = new ServerStyleSheet();
     const page = props.renderPage(App => props =>
-      sheet.collectStyles(
-        <StyletronProvider value={styletron}>
-          <App {...props} />
-        </StyletronProvider>
-      )
+      sheet.collectStyles(<App {...props} />)
     );
     const styleTags = sheet.getStyleElement();
 
-    const stylesheets = styletron.getStylesheets() || [];
-    return { ...page, stylesheets, styleTags };
+    return { ...page, styleTags };
   }
 
   render() {
@@ -34,15 +27,6 @@ class MyDocument extends Document {
               'html, body, p, ol, ul, li, dl, dt, dd, blockquote, figure, fieldset, legend, textarea, pre, iframe, hr, h1, h2, h3, h4, h5, h6 { margin: 0; padding: 0; } h1, h2, h3, h4, h5, h6 { font-size: 100%; font-weight: normal; } ul { list-style: none; } button, input, select, textarea { margin: 0; } html { box-sizing: border-box; } *, *:before, *:after { box-sizing: inherit; } img, embed, iframe, object, video { height: auto; max-width: 100%; } audio { max-width: 100%; } iframe { border: 0; } table { border-collapse: collapse; border-spacing: 0; } td, th { padding: 0; text-align: left; }'
             }
           </style>
-          {this.props.stylesheets.map((sheet, i) => (
-            <style
-              className="_styletron_hydrate_"
-              dangerouslySetInnerHTML={{ __html: sheet.css }}
-              media={sheet.attrs.media}
-              data-hydrate={sheet.attrs['data-hydrate']}
-              key={i}
-            />
-          ))}
           {this.props.styleTags}
         </Head>
         <body>

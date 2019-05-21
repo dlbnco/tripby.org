915f1a8978a0ebda09af06fb923451d0c856541c
Mateus Dal Bianco <mateus@dalbinaco.com>
2019-05-21 14:19:46 +0200

    Wrap with <Card />

--- components/Substance/ROAs/index.js
+++ components/Substance/ROAs/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Flex, Box } from 'rebass';
 import RoaSection from './Section';
+import { FormattedMessage } from 'react-intl';
+import Card from '../../Card';
+import Text from '../../Text';
 
 const SubstanceRoas = ({ substance, ...props }) => {
   return (
-    <Flex flexDirection="column" {...props} m={-2}>
-      {substance.roas.map(roa => (
-        <Box p={2} key={`${substance.name}-roa-${roa.name}`}>
-          <RoaSection roa={roa} />
-        </Box>
-      ))}
-    </Flex>
+    <Card shadow={false}>
+      <Text fontSize={0} mb={2} variant="secondary">
+        <FormattedMessage id="Substance.roas" />
+      </Text>
+      <Flex flexDirection="column" {...props} m={-2}>
+        {substance.roas.map(roa => (
+          <Box p={2} key={`${substance.name}-roa-${roa.name}`}>
+            <RoaSection roa={roa} />
+          </Box>
+        ))}
+      </Flex>
+    </Card>
   );
 };
 

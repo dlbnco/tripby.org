7b3da644cf26fca774c91afb8afc3629825fa1ab
Mateus Dal Bianco <mateus@dalbinaco.com>
2019-05-28 16:29:16 +0200

    Set up next-ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import App, { Container } from 'next/app';
 import React from 'react';
-import withApolloClient from '../lib/with-apollo-client';
+import Router from 'next/router';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
 import { ThemeProvider } from 'styled-components';
+import withGA from 'next-ga';
 
+import withApolloClient from '../lib/with-apollo-client';
 import Layout from '../components/Layout';
 import { defaultTheme } from '../lib/theme';
 import IntlProvider from '../components/IntlProvider';
+import { GA_TRACKING_ID } from '../lib/constants';
 
 class MyApp extends App {
   render() {
@@ -30,4 +33,4 @@ class MyApp extends App {
   }
 }
 
-export default withApolloClient(MyApp);
+export default withGA(GA_TRACKING_ID, Router)(withApolloClient(MyApp));

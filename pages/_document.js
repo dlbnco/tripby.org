90dfcf658bf5fe09870e7b5d8f5539fd9f9ca8cb
Mateus Dal Bianco <mateus@dalbinaco.com>
2019-05-28 12:09:37 +0200

    Add keys on Head

--- pages/_document.js
+++ pages/_document.js
@@ -20,9 +20,10 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css?family=Barlow:300,400,500,700,800"
             rel="stylesheet"
+            key="googlefonts"
           />
           {/* minireset.css */}
-          <style type="text/css">
+          <style type="text/css" key="minireset">
             {
               'html, body, p, ol, ul, li, dl, dt, dd, blockquote, figure, fieldset, legend, textarea, pre, iframe, hr, h1, h2, h3, h4, h5, h6 { margin: 0; padding: 0; } h1, h2, h3, h4, h5, h6 { font-size: 100%; font-weight: normal; } ul { list-style: none; } button, input, select, textarea { margin: 0; } html { box-sizing: border-box; } *, *:before, *:after { box-sizing: inherit; } img, embed, iframe, object, video { height: auto; max-width: 100%; } audio { max-width: 100%; } iframe { border: 0; } table { border-collapse: collapse; border-spacing: 0; } td, th { padding: 0; text-align: left; }'
             }

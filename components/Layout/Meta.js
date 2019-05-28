deb43c02b638f8e13a54e553c06bad051c2cdaea
Mateus Dal Bianco <mateus@dalbinaco.com>
2019-05-28 13:52:32 +0200

    Include favicon

--- components/Layout/Meta.js
+++ components/Layout/Meta.js
@@ -21,6 +21,32 @@ const AppMeta = () => {
             content={description}
           />
           <meta name="og:title" key="og:title" content={title} />
+          {/* favicon 👇 */}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/static/safari-pinned-tab.svg"
+            color="#6e31cc"
+          />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
       )}
     </HeadMessages>

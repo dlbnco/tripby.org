74e9a948c069af189900745223705b1e84dfa3fa
Mateus Dal Bianco <mateus@dlbn.co>
2019-06-03 11:24:52 +0200

    Do not list the own substance in its related substances

--- components/Substance/Related/index.js
+++ components/Substance/Related/index.js
@@ -12,9 +12,12 @@ import { FormattedMessage } from 'react-intl';
 const SubstanceRelated = ({ substance }) => {
   const { data } = useQuery(GET_SUBSTANCES, { variables: { limit: 300 } });
   if (data && data.substances) {
+    const otherSubstances = data.substances.filter(
+      sub => sub.name !== substance.name
+    );
     const relatedSubstancesByChemicalClass =
       substance.class && substance.class.chemical
-        ? data.substances.filter(
+        ? otherSubstances.filter(
             _substance =>
               _substance.class &&
               _substance.class.chemical &&
@@ -25,7 +28,7 @@ const SubstanceRelated = ({ substance }) => {
         : [];
     const relatedSubstancesByPsychoactiveClass =
       substance.class && substance.class.psychoactive
-        ? data.substances.filter(
+        ? otherSubstances.filter(
             _substance =>
               _substance.class &&
               _substance.class.psychoactive &&
@@ -54,16 +57,19 @@ const SubstanceRelated = ({ substance }) => {
         value: relatedSubstancesByPsychoactiveClass,
       },
     ];
+    const contentfulRelatedSubstances = relatedSubstances.filter(
+      cat => cat.value.length > 0
+    );
     return (
       <Tabs>
         <StyledTabList m={-1} pb={3}>
-          {relatedSubstances.map(cat => (
+          {contentfulRelatedSubstances.map(cat => (
             <StyledTab p={1} key={`${cat.id}-tab`}>
               <FormattedMessage id={cat.id} />
             </StyledTab>
           ))}
         </StyledTabList>
-        {relatedSubstances.map(cat => (
+        {contentfulRelatedSubstances.map(cat => (
           <TabPanel key={`${cat.id}-content`}>
             <Flex flexWrap="wrap" m={-1}>
               {cat.value.map(relatedSubstance => (

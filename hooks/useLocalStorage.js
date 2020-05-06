02e671d4a28d3df681008589cdc0ea8138138d06
Mateus Dal Bianco <mateus@dlbn.co>
2020-05-06 11:35:37 -0300

    fix(useLocalStorage): check for window

--- hooks/useLocalStorage.js
+++ hooks/useLocalStorage.js
@@ -10,16 +10,16 @@ const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(() => {
     try {
       // Get from local storage by key
-
-      const item = window.localStorage.getItem(key);
+      let item;
+      if (typeof window !== 'undefined') {
+        item = window.localStorage.getItem(key);
+      }
 
       // Parse stored json or if none return initialValue
 
       return item ? JSON.parse(item) : initialValue;
     } catch (error) {
       // If error also return initialValue
-
-      console.log(error);
 
       return initialValue;
     }
@@ -29,7 +29,7 @@ const useLocalStorage = (key, initialValue) => {
 
   // ... persists the new value to localStorage.
 
-  const setValue = value => {
+  const setValue = (value) => {
     try {
       // Allow value to be a function so we have same API as useState
 
@@ -41,8 +41,9 @@ const useLocalStorage = (key, initialValue) => {
       setStoredValue(valueToStore);
 
       // Save to local storage
-
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
     } catch (error) {
       // A more advanced implementation would handle the error case
 
